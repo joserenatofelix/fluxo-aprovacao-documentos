b10672314b3e1f98a4adc5dcd65ae3ead683a869
José Renato Felix da Silva <93296913+joserenatofelix@users.noreply.github.com>
2024-12-27 17:53:07 -0300

    update register

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./../styles/Register.css";
 
 const Register = () => {
   const handleRegister = (e) => {
@@ -10,8 +11,12 @@ const Register = () => {
     <div>
       <h1>Registrar</h1>
       <form onSubmit={handleRegister}>
+        <input type="text" placeholder="Nome completo" required />
         <input type="text" placeholder="Nome de usuário" required />
         <input type="password" placeholder="Senha" required />
+        <input type="email" placeholder="E-mail" required />
+        <input type="text" placeholder="CPF" required />
+        <input type="text" placeholder="Função" required />
         <button type="submit">Registrar</button>
       </form>
     </div>
